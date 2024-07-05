be8d5857ff6020bff917108405ac1c39414454a5
Cidralll <lucas.woginiack@yahoo.com.br>
2024-07-05 17:19:55 -0300

    bug fix

--- src/api/getPosts.ts
+++ src/api/getPosts.ts
@@ -1,7 +1,7 @@
 export async function getPosts() {
     try {
         // a proprietade revalidate diminui a quantidade de solicitacoes | cache
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=%7B%22type%22:%22posts%22%7D&limit=10&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`, { next: { revalidate: 120 } });
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=%7B%22type%22:%22posts%22%7D&limit=10&read_key=${process.env.NEXT_PUBLIC_READ_KEY}&depth=1&props=slug,title,metadata`, { next: { revalidate: 120 } });
         
         if (!res.ok) {
             throw new Error('Failed to fetch data');
--- src/api/getProfileBanner.ts
+++ src/api/getProfileBanner.ts
@@ -1,7 +1,7 @@
 export async function getProfileBanner() {
     try {
         // a proprietade revalidate diminui a quantidade de solicitacoes | cache
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/6687e16c328da0e2573f9cfa?read_key=${process.env.READ_KEY}&depth=1&props=metadata`, { next: { revalidate: 120 } });
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/6687e16c328da0e2573f9cfa?read_key=${process.env.NEXT_PUBLIC_READ_KEY}&depth=1&props=metadata`, { next: { revalidate: 120 } });
 
         if (!res.ok) {
             throw new Error('Failed to fetch data');
