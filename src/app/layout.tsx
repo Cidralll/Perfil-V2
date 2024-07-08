21a5d612cdbe311c2943e59382fe718f8147b367
Cidralll <lucas.woginiack@yahoo.com.br>
2024-07-08 09:47:38 -0300

    GOOGLE ANALYTICS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import GoogleAnalytics from "@/components/analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+            <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+          ) : <></>}
+        {children}
+      </body>
     </html>
   );
 }
